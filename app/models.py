e92db046ed7ac981e49ad1cc40bd6250dd2685ca
Matthew <matthewv@thesecretlocation.com>
2017-06-29 16:44:45 -0500

    Commiting v4

--- app/models.py
+++ app/models.py
@@ -164,7 +164,7 @@ class FrontPageLinks(models.Model):
     def __str__(self):
         return self.url
 
-class MemberNewsItem(models.Model):
+class NewsItem(models.Model):
 
     id = models.AutoField(primary_key=True)
 
